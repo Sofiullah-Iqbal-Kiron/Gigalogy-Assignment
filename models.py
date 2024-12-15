669dd2d173b284ea0a9ad7e80ec2456d6ad69f2f
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2024-12-15 17:51:33 +0600

    add models, utils and docker

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
