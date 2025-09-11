2de7a3ca6e4f8c2f584eaf146dec3127c4ee2aa1
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2025-09-11 12:33:43 +0600

    latest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # fastapi
 from fastapi import FastAPI
 
-# 3'rd party
-# from sqladmin import Admin, ModelView
 
 # local
 from src.core.db import Base, engine
@@ -15,31 +13,3 @@ app = FastAPI(title="User Registration and Email Verification System")
 
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["auth"])
 app.include_router(user.router, prefix="/api/v1/users", tags=["users"])
-
-
-# class UserAdmin(ModelView):
-#     model = User
-#     column_list = [
-#         "id",
-#         "email",
-#         "first_name",
-#         "last_name",
-#         "is_active",
-#         "is_superuser",
-#         "joined_at",
-#     ]
-#     column_searchable_list = ["email", "first_name", "last_name"]
-#     column_sortable_list = [
-#         "id",
-#         "email",
-#         "first_name",
-#         "last_name",
-#         "is_active",
-#         "is_superuser",
-#         "joined_at",
-#     ]
-#     form_excluded_columns = ["hashed_password", "last_login"]
-
-
-# admin = Admin(app, engine)
-# admin.add_view(UserAdmin)
